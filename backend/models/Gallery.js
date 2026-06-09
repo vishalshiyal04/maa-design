6d4f0165eaef01e76a321d0946bed6b69a3fbc5c
Vishal B Shiyal <vishalbshiyal@gmail.com>
2026-06-09 19:00:29 +0530

    Fix: Permanently remove node_modules from git tracking

---
+++ backend/models/Gallery.js
@@ -0,0 +1,8 @@
+// const mongoose = require("mongoose");
+// const gallerySchema = new mongoose.Schema({
+//   title: String,
+//   category: String,
+//   image: String,
+//   createdAt: { type: Date, default: Date.now },
+// });
+// module.exports = mongoose.model("Gallery", gallerySchema);
---
+++ backend/models/Inquiry.js
@@ -0,0 +1,10 @@
+// const mongoose = require("mongoose");
+// const inquirySchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   phone: String,
+//   message: String,
+//   status: { type: String, default: "Pending" },
+//   createdAt: { type: Date, default: Date.now },
+// });
+// module.exports = mongoose.model("Inquiry", inquirySchema);
